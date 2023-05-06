526a5aeebb7b9f6250ca9cb84722c51aa7b536ea
Chehab-MOSAAD <chehab.mosaad@umiv-tlse3.fr>
2023-05-06 16:56:17 +0200

    ITER_7

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -2,32 +2,27 @@ package ilu2;
 
 public class Welcome {
 	
-	public static String Welcome(String name) {
-	    if (name == null || name.trim().isEmpty()) {
+	public static String Welcome(String names) {
+	    if (names == null || names.trim().isEmpty()) {
 	        return "Hello, my friend";
-	    } else if (name.equals(name.toUpperCase())) {
-	        return "HELLO, " + name.toUpperCase() + " !";
-	    } else if (name.contains(",")) {
-	        String[] names = name.split(",");
+	    } else if (names.equals(names.toUpperCase())) {
+	        return "HELLO, " + names.toUpperCase() + " !";
+	    } else if (names.contains(",")) {
+	        String[] nameArray = names.split(",");
 	        StringBuilder sb = new StringBuilder();
-	        StringBuilder sb2 = new StringBuilder();
-	        for (String n : names) {
-	            String capitalized = n.trim().substring(0, 1).toUpperCase() + n.trim().substring(1);
-	            if (n.equals(n.toUpperCase())) {
-	                sb2.append(capitalized).append(" ");
-	            } else {
-	                sb.append(", ").append(capitalized);
+	        for (int i = 0; i < nameArray.length; i++) {
+	            String name = nameArray[i].trim();
+	            String capitalized = name.substring(0, 1).toUpperCase() + name.substring(1);
+	            sb.append(capitalized);
+	            if (i == nameArray.length - 2) {
+	                sb.append(" and ");
+	            } else if (i < nameArray.length - 1) {
+	                sb.append(", ");
 	            }
 	        }
-	        String result1 = "Hello" + sb.toString();
-	        if (sb2.length() > 0) {
-	            String result2 = "AND HELLO, " + sb2.toString().trim().toUpperCase() + " !";
-	            return result1 + ". " + result2;
-	        } else {
-	            return result1;
-	        }
+	        return "Hello, " + sb.toString();
 	    } else {
-	        String capitalized = name.substring(0, 1).toUpperCase() + name.substring(1);
+	        String capitalized = names.substring(0, 1).toUpperCase() + names.substring(1);
 	        return "Hello, " + capitalized;
 	    }
 	}
