0a795c7718c4e287bba9bb2dfd27ae31c61a5b16
Chehab-MOSAAD <chehab.mosaad@umiv-tlse3.fr>
2023-05-06 17:02:08 +0200

    ITER_8

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -1,28 +1,55 @@
 package ilu2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Welcome {
 	
-	public static String Welcome(String names) {
-	    if (names == null || names.trim().isEmpty()) {
+	public static String Welcome(String name) {
+	    if (name == null || name.trim().isEmpty()) {
 	        return "Hello, my friend";
-	    } else if (names.equals(names.toUpperCase())) {
-	        return "HELLO, " + names.toUpperCase() + " !";
-	    } else if (names.contains(",")) {
-	        String[] nameArray = names.split(",");
-	        StringBuilder sb = new StringBuilder();
-	        for (int i = 0; i < nameArray.length; i++) {
-	            String name = nameArray[i].trim();
-	            String capitalized = name.substring(0, 1).toUpperCase() + name.substring(1);
-	            sb.append(capitalized);
-	            if (i == nameArray.length - 2) {
-	                sb.append(" and ");
-	            } else if (i < nameArray.length - 1) {
-	                sb.append(", ");
+	    } else if (name.equals(name.toUpperCase())) {
+	        return "HELLO, " + name.toUpperCase() + " !";
+	    } else if (name.contains(",")) {
+	        String[] names = name.split(",");
+	        List<String> capitalizedNames = new ArrayList<>();
+	        List<String> uppercaseNames = new ArrayList<>();
+	        for (String n : names) {
+	            String trimmedName = n.trim();
+	            if (trimmedName.equals(trimmedName.toUpperCase())) {
+	                uppercaseNames.add(trimmedName);
+	            } else {
+	                capitalizedNames.add(trimmedName.substring(0, 1).toUpperCase() + trimmedName.substring(1));
 	            }
 	        }
-	        return "Hello, " + sb.toString();
+
+	        StringBuilder sb = new StringBuilder("Hello");
+	        if (!capitalizedNames.isEmpty()) {
+	            for (int i = 0; i < capitalizedNames.size() - 1; i++) {
+	                sb.append(", ").append(capitalizedNames.get(i));
+	            }
+	            if (capitalizedNames.size() > 1) {
+	                sb.append(" and ");
+	            }
+	            sb.append(capitalizedNames.get(capitalizedNames.size() - 1));
+	        }
+
+	        if (!uppercaseNames.isEmpty()) {
+	            if (uppercaseNames.size() > 1) {
+	                sb.append(". AND HELLO, ");
+	                for (int i = 0; i < uppercaseNames.size() - 1; i++) {
+	                    sb.append(uppercaseNames.get(i)).append(" ");
+	                }
+	                sb.append("AND ");
+	            } else {
+	                sb.append(". AND HELLO, ");
+	            }
+	            sb.append(uppercaseNames.get(uppercaseNames.size() - 1)).append(" !");
+	        }
+
+	        return sb.toString();
 	    } else {
-	        String capitalized = names.substring(0, 1).toUpperCase() + names.substring(1);
+	        String capitalized = name.substring(0, 1).toUpperCase() + name.substring(1);
 	        return "Hello, " + capitalized;
 	    }
 	}
