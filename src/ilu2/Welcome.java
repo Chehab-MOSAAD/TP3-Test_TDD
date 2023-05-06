edf2ec8fa4dadcad8112895031bf67613da480ad
Chehab-MOSAAD <chehab.mosaad@umiv-tlse3.fr>
2023-05-06 16:53:28 +0200

    ITER_6

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -10,11 +10,22 @@ public class Welcome {
 	    } else if (name.contains(",")) {
 	        String[] names = name.split(",");
 	        StringBuilder sb = new StringBuilder();
+	        StringBuilder sb2 = new StringBuilder();
 	        for (String n : names) {
 	            String capitalized = n.trim().substring(0, 1).toUpperCase() + n.trim().substring(1);
-	            sb.append(", ").append(capitalized);
+	            if (n.equals(n.toUpperCase())) {
+	                sb2.append(capitalized).append(" ");
+	            } else {
+	                sb.append(", ").append(capitalized);
+	            }
 	        }
-	        return "Hello" + sb.toString();
+	        String result1 = "Hello" + sb.toString();
+	        if (sb2.length() > 0) {
+	            String result2 = "AND HELLO, " + sb2.toString().trim().toUpperCase() + " !";
+	            return result1 + ". " + result2;
+	        } else {
+	            return result1;
+	        }
 	    } else {
 	        String capitalized = name.substring(0, 1).toUpperCase() + name.substring(1);
 	        return "Hello, " + capitalized;
