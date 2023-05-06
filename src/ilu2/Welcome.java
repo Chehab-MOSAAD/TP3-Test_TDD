fceebef96248df2b795ff751a92c2adb4e0abe59
Chehab-MOSAAD <chehab.mosaad@umiv-tlse3.fr>
2023-05-06 17:06:46 +0200

    ITER_9

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -11,55 +11,71 @@ public class Welcome {
 	    if (name == null || name.trim().isEmpty()) {
 	        return "Hello, my friend";
 	    } else if (name.equals(name.toUpperCase())) {
-	        return "HELLO, " + name.toUpperCase() + " !";
+	        return uppercaseGreeting(name);
 	    } else if (name.contains(",")) {
-	        String[] names = name.split(",");
-	        Map<String, Integer> counts = new LinkedHashMap<>();
-	        for (String n : names) {
-	            String trimmedName = n.trim();
-	            counts.put(trimmedName, counts.getOrDefault(trimmedName, 0) + 1);
-	        }
-
-	        List<String> capitalizedNames = new ArrayList<>();
-	        List<String> uppercaseNames = new ArrayList<>();
-	        for (Map.Entry<String, Integer> entry : counts.entrySet()) {
-	            String n = entry.getKey();
-	            Integer count = entry.getValue();
-	            if (n.equals(n.toUpperCase())) {
-	                uppercaseNames.add(n + (count > 1 ? " (x" + count + ")" : ""));
-	            } else {
-	                capitalizedNames.add(n.substring(0, 1).toUpperCase() + n.substring(1) + (count > 1 ? " (x" + count + ")" : ""));
-	            }
-	        }
-
-	        StringBuilder sb = new StringBuilder("Hello");
-	        if (!capitalizedNames.isEmpty()) {
-	            for (int i = 0; i < capitalizedNames.size() - 1; i++) {
-	                sb.append(", ").append(capitalizedNames.get(i));
-	            }
-	            if (capitalizedNames.size() > 1) {
-	                sb.append(" and ");
-	            }
-	            sb.append(capitalizedNames.get(capitalizedNames.size() - 1));
-	        }
-
-	        if (!uppercaseNames.isEmpty()) {
-	            if (uppercaseNames.size() > 1) {
-	                sb.append(". AND HELLO, ");
-	                for (int i = 0; i < uppercaseNames.size() - 1; i++) {
-	                    sb.append(uppercaseNames.get(i)).append(" ");
-	                }
-	                sb.append("AND ");
-	            } else {
-	                sb.append(". AND HELLO, ");
-	            }
-	            sb.append(uppercaseNames.get(uppercaseNames.size() - 1)).append(" !");
-	        }
-
-	        return sb.toString();
+	        return multiNameGreeting(name);
 	    } else {
-	        String capitalized = name.substring(0, 1).toUpperCase() + name.substring(1);
-	        return "Hello, " + capitalized;
+	        return capitalizeGreeting(name);
 	    }
+	}
+
+	private static String capitalizeGreeting(String name) {
+	    String capitalized = name.substring(0, 1).toUpperCase() + name.substring(1);
+	    return "Hello, " + capitalized;
+	}
+
+	private static String uppercaseGreeting(String name) {
+	    return "HELLO, " + name.toUpperCase() + " !";
+	}
+
+	private static String multiNameGreeting(String name) {
+	    String[] names = name.split(",");
+	    Map<String, Integer> counts = new LinkedHashMap<>();
+	    for (String n : names) {
+	        String trimmedName = n.trim();
+	        counts.put(trimmedName, counts.getOrDefault(trimmedName, 0) + 1);
+	    }
+
+	    List<String> capitalizedNames = new ArrayList<>();
+	    List<String> uppercaseNames = new ArrayList<>();
+	    for (Map.Entry<String, Integer> entry : counts.entrySet()) {
+	        String n = entry.getKey();
+	        Integer count = entry.getValue();
+	        if (n.equals(n.toUpperCase())) {
+	            uppercaseNames.add(n + (count > 1 ? " (x" + count + ")" : ""));
+	        } else {
+	            capitalizedNames.add(n.substring(0, 1).toUpperCase() + n.substring(1) + (count > 1 ? " (x" + count + ")" : ""));
+	        }
+	    }
+
+	    return multiNameGreetingBuilder(capitalizedNames, uppercaseNames);
+	}
+
+	private static String multiNameGreetingBuilder(List<String> capitalizedNames, List<String> uppercaseNames) {
+	    StringBuilder sb = new StringBuilder("Hello");
+	    if (!capitalizedNames.isEmpty()) {
+	        for (int i = 0; i < capitalizedNames.size() - 1; i++) {
+	            sb.append(", ").append(capitalizedNames.get(i));
+	        }
+	        if (capitalizedNames.size() > 1) {
+	            sb.append(" and ");
+	        }
+	        sb.append(capitalizedNames.get(capitalizedNames.size() - 1));
+	    }
+
+	    if (!uppercaseNames.isEmpty()) {
+	        if (uppercaseNames.size() > 1) {
+	            sb.append(". AND HELLO, ");
+	            for (int i = 0; i < uppercaseNames.size() - 1; i++) {
+	                sb.append(uppercaseNames.get(i)).append(" ");
+	            }
+	            sb.append("AND ");
+	        } else {
+	            sb.append(". AND HELLO, ");
+	        }
+	        sb.append(uppercaseNames.get(uppercaseNames.size() - 1)).append(" !");
+	    }
+
+	    return sb.toString();
 	}
 }
--- test/ilu2_test/WelcomeTest.java
+++ test/ilu2_test/WelcomeTest.java
@@ -35,19 +35,19 @@ public class WelcomeTest {
     @Test
     public void testWelcomeCase4() {
         String result = Welcome.Welcome("amy,bob");
-        assertEquals("Hello, Amy, Bob", result);
+        assertEquals("Hello, Amy and Bob", result);
     }
 
     @Test
     public void testWelcomeCase5() {
         String result = Welcome.Welcome("Amy, bob,jerry");
-        assertEquals("Hello, Amy, Bob, Jerry", result);
+        assertEquals("Hello, Amy, Bob and Jerry", result);
     }
 
     @Test
     public void testWelcomeCase6() {;
         String result = Welcome.Welcome("Amy, BOB, Jerry");
-        assertEquals("Hello, Amy, Jerry. AND HELLO, BOB !", result);
+        assertEquals("Hello, Amy and Jerry. AND HELLO, BOB !", result);
     }
 
     @Test
@@ -71,17 +71,23 @@ public class WelcomeTest {
     @Test
     public void testWelcomeCase10() {
         String result = Welcome.Welcome("bob, JERRY, amy, bob, JERRY, bob");
-        assertEquals("Hello, Bob (x3), and Amy. AND HELLO, JERRY (x2) !", result);
+        assertEquals("Hello, Bob (x3) and Amy. AND HELLO, JERRY (x2) !", result);
     }
 
     @Test
     public void testWelcomeCase11() {
+        String result = Welcome.Welcome("bob, yoda, amy");
+        assertEquals("Bob, Yoda, and Amy, Hello.", result);
+    }
+    
+    @Test
+    public void testWelcomeCase12() {
         String result = Welcome.Welcome("bob, yoda, amy, JERRY");
-        assertEquals("Bob, Yoda, and Amy, Hello. AND HELLO JERRY", result);
+        assertEquals("Bob, Yoda, and Amy, Hello. AND HELLO, JERRY!", result);
     }
 
     @Test
-    public void testWelcomeCase12() {
+    public void testWelcomeCase13() {
         String result = Welcome.Welcome("bob, YODA, amy, JERRY, YODA");
         assertEquals("Hello, Bob and Amy. AND YODA (x2) AND JERRY, HELLO !", result);
     }
