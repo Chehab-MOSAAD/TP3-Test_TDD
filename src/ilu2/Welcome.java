e8858596079a6d307e301271d9df2cc405c505b8
Chehab-MOSAAD <chehab.mosaad@umiv-tlse3.fr>
2023-05-06 16:50:59 +0200

    ITER_4

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -7,6 +7,11 @@ public class Welcome {
 	        return "Hello, my friend";
 	    } else if (name.equals(name.toUpperCase())) {
 	        return "HELLO, " + name.toUpperCase() + " !";
+	    } else if (name.contains(",")) {
+	        String[] names = name.split(",");
+	        String capitalized1 = names[0].substring(0, 1).toUpperCase() + names[0].substring(1);
+	        String capitalized2 = names[1].substring(0, 1).toUpperCase() + names[1].substring(1);
+	        return "Hello, " + capitalized1 + ", " + capitalized2;
 	    } else {
 	        String capitalized = name.substring(0, 1).toUpperCase() + name.substring(1);
 	        return "Hello, " + capitalized;
